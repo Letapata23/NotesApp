4cb8b7997abaeb89206c8cbaf78df374a00275f4
Letapata <Letapataqhobela@gmail.com>
2025-07-26 11:41:12 +0200

    delete note functionality created

--- src/app/show-note/show-note.component.ts
+++ src/app/show-note/show-note.component.ts
@@ -14,6 +14,7 @@ import { Note } from '../notes';
             <li>{{note.text}}</li>
         </ul>
     </div>
+    <button (click) = "deleteNote()">Delete</button>
   `,
   styleUrl: './show-note.component.css'
 })
@@ -22,12 +23,24 @@ export class ShowNoteComponent {
   notes:Note[] 
   id:number 
   note:Note 
+  router:Router
 
   constructor(){
     this.activeRoute = inject(ActivatedRoute)
     this.notes = notes
     this.id = Number(this.activeRoute.snapshot.paramMap.get('id'))
     this.note = notes.find((note) => note.id === this.id)!
+    this.router = inject(Router)
+  }
+
+  deleteNote(){
+    var id = this.notes.indexOf(this.note,0)
+    
+    if(id != -1){
+      notes.splice(id,1)
+      this.router.navigateByUrl('')
+    }
+
   }
   
 }
